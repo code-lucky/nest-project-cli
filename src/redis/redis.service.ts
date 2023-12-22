de13e74986305cc4a4d4cb6cafd920d6037baf8f
code-lucky <2924249993@qq.com>
2023-12-22 16:21:50 +0800

    修改成ioredis连接

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,21 +1,17 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { RedisClientType } from 'redis';
+import { Injectable } from '@nestjs/common';
+import type { OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import Redis from 'ioredis';
 
 @Injectable()
-export class RedisService {
+export class RedisService extends Redis{
 
-    @Inject('REDIS_CLIENT') 
-    private redisClient: RedisClientType;
-
-    async get(key: string) {
-        return await this.redisClient.get(key);
-    }
-
-    async set(key: string, value: string | number, ttl?: number) {
-        await this.redisClient.set(key, value);
-
-        if(ttl) {
-            await this.redisClient.expire(key, ttl);
-        }
+    constructor(private configService: ConfigService){
+        super({
+            port: configService.get('redis_server_port'),
+            host: configService.get('redis_server_host'),
+            password: configService.get('redis_server_password'),
+            lazyConnect: true
+        })
     }
 }
